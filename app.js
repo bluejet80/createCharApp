deda7a119aa39ed02a7fa9161b21512ed278338e
Jarett Young <know_jarett@yahoo.com>
2022-11-13 20:37:57 -0600

    setting up api

--- app.js
+++ app.js
@@ -1,10 +1,21 @@
 'use strict';
 
+const fs = require('fs');
 const express = require('express');
 const morgan = require('morgan');
 const { json } = require('express/lib/response');
 
 const app = express();
+
+app.use(express.json());
+
+// get data
+
+const character = JSON.parse(
+  fs.readFileSync(`${__dirname}/data/characters.json`)
+);
+
+// routes
 
 app.get('/', (req, res) => {
   res.status(200).json({
@@ -17,6 +28,19 @@ app.post('/', (req, res) => {
   res.send('you can post to this endpoint.');
 });
 
-app.get('/api/v1/chars', (req, res) => {});
+app.get('/api/v1/chars', (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    results: character.length,
+    data: { character },
+  });
+});
+
+app.post('/api/v1/chars', (req, res) => {
+  console.log(req.body);
+  res.send('Done');
+});
+
+// export
 
 module.exports = app;
